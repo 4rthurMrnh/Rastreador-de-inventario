cb60e74ed4c846372abab84b7bd3cd0df1b93cac
Arthur H Marinho <ykhalloween4@gmail.com>
2025-09-29 22:48:28 -0300

    Criação da lógica do negócio e ínicio da database
    Implementei a lógica de como irá funcionar o negócio utilizando Option e Result presentes no Rust, para ter um retorno de onde está o erro caso ocorra.
    Comecei também a criação do banco de dados.

--- inventory-tracker/src/models.rs
+++ inventory-tracker/src/models.rs
@@ -2,6 +2,7 @@ pub struct Produto {
     pub id: i32,
     pub nome: String,
     pub preco: f64,
+    pub estoque: i32,
 }
 
 pub struct VendaHistoricos {
@@ -9,4 +10,12 @@ pub struct VendaHistoricos {
     pub produto_id: i32,
     pub data_venda: String,
     pub quantidade: i32,
+}
+
+#[derive(Debug)]
+pub enum ErroEstoque {
+    ProdutoNaoEncontrado,
+    QuantidadeInsuficiente,
+    ValorInvalido,
+    EstoqueInsuficiente,
 }
